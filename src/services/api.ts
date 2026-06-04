9c110c775da3679d7b94367f930ff8dee3f5d09b
Hector Paramo <hector.paramo@g-global.com>
2026-06-04 11:29:56 -0700

    feat: integrate dotenv for environment variable management and add API_BASE_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const API_BASE_URL = 'http://localhost:3000';
+import { API_BASE_URL } from '@env';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
